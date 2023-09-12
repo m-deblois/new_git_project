3b1f71c113c2386bc93a13afc5e0e712330a162a
Madeleine deBlois <mdeblois@arizona.edu>
2023-09-12 12:58:23 -0700

    cat data

---
+++ cat_data.R
@@ -0,0 +1,5 @@
+#making new pretend dataset
+new_data <- data.frame(types = c("tortie", "calico", "halloween"), 
+                       color=c("multi", "multi", "midnight"))
+                       
+write.csv(new_data, "cat_colors.csv")
