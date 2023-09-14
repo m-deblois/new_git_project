fdf1a03453c4a54b7797414838d85169dd808163
Madeleine deBlois <mdeblois@arizona.edu>
2023-09-14 11:31:17 -0700

    greetings added

--- cat_data.R
+++ cat_data.R
@@ -3,3 +3,5 @@ new_data <- data.frame(types = c("tortie", "calico", "halloween"),
                        color=c("multi", "multi", "midnight"))
                        
 write.csv(new_data, "cat_colors.csv")
+
+print("hello!")
